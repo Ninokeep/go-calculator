2aeb34c033092b0c6e4efb2c54608a10fa0b3cf6
fababy <studev170@gmail.com>
2023-08-24 22:03:49 +0200

    Fix division

--- app/main.go
+++ app/main.go
@@ -11,40 +11,45 @@ func main(){
 	var operator string
 
 
-	fmt.Print("Enter a number ...")
-	fmt.Scan(&num1)
+	fmt.Println("Enter a number ...")
+	fmt.Scan(&num1);
 
-	fmt.Scan(&operator)
+	for {
+
+	fmt.Scan(&operator);
+
+	if operator == " " {
+		break;
+	}
 
 	switch operator {
 
 	case "-":
-		fmt.Scan(&num2)
-		num1 -= num2
-		fmt.Print(num1)
+		fmt.Scan(&num2);
+		num1 -= num2;
+		fmt.Print(num1);
 
 	case "*":
-		fmt.Scan(&num2)
-		num1 *= num2
-		fmt.Print(num1)
+		fmt.Scan(&num2);
+		num1 *= num2;
+		fmt.Print(num1);
 
 	case "+":
-		fmt.Scan(&num2)
-		num1 += num2
-		fmt.Print(num1)
+		fmt.Scan(&num2);
+		num1 += num2;
+		fmt.Print(num1);
 
 	case "/":
-		fmt.Scan(&num2)
+		fmt.Scan(&num2);
 
 		if(num2 == 0){
-			fmt.Print(errors.New("You cannot divided by 0"))
-			break
+			fmt.Print(errors.New("You cannot divided by 0"));
+			break;
 		}
-		num1 += num2
-		fmt.Print(num1)
+		num1 /= num2;
+		fmt.Print(num1);
 	}
-
-
+	}
 
 }
 
