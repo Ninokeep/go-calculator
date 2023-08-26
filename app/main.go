c9957e7fb328289fe07a16ebff1602ff58db4ee7
fababy <studev170@gmail.com>
2023-08-26 08:03:57 +0200

    translate the output float 64 result by string

--- app/main.go
+++ app/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 func main(){
 
 	var num1, num2 float64
 	var operator string
-
-
 	fmt.Println("Enter a number ...")
+
 	fmt.Scan(&num1);
 
 	for {
 
-	fmt.Scan(&operator);
+	fmt.Scanln(&operator);
 
 	if operator == " " {
 		break;
@@ -25,33 +25,34 @@ func main(){
 	switch operator {
 
 	case "-":
-		fmt.Scan(&num2);
+		fmt.Scanln(&num2);
 		num1 -= num2;
-		fmt.Print(num1);
+		getTheResult(num1)
 
 	case "*":
-		fmt.Scan(&num2);
+		fmt.Scanln(&num2);
 		num1 *= num2;
-		fmt.Print(num1);
-
+		getTheResult(num1)
 	case "+":
-		fmt.Scan(&num2);
+		fmt.Scanln(&num2);
 		num1 += num2;
-		fmt.Print(num1);
+		getTheResult(num1)
 
 	case "/":
-		fmt.Scan(&num2);
+		fmt.Scanln(&num2);
 
 		if(num2 == 0){
 			fmt.Print(errors.New("You cannot divided by 0"));
 			break;
 		}
 		num1 /= num2;
-		fmt.Print(num1);
+		getTheResult(num1)
 	}
 	}
 
 }
 
+func getTheResult(num float64)  {
 
-
+ fmt.Printf("Total : %s", strconv.FormatFloat(num, 'f', -1, 64));
+}
